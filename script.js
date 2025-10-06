c19bd0d8addf8f8edfa1267cc8379a7c9209cfaa
barbabarros <barbabarros@windowslive.com>
2025-10-05 22:28:13 -0300

    teste

--- script.js
+++ script.js
@@ -167,8 +167,6 @@ ${usarWebSocketPrincipal ? `
         proxy_buffer_size 128k;
         proxy_buffers 4 256k;
         proxy_busy_buffers_size 256k;
-    }
-
 ${usarWebSocket ? `
         # Suporte para WebSocket
         proxy_http_version 1.1;
